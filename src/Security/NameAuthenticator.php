951fce80a9a5a0afb3dd5cb20cf4f069f8cbcda1
I4got2eat <mykolassandag@gmail.com>
2026-03-09 17:46:27 +0200

    fix

--- src/Security/NameAuthenticator.php
+++ src/Security/NameAuthenticator.php
@@ -17,9 +17,10 @@ use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPassport;
+use Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface;
 use Symfony\Component\Security\Http\Util\TargetPathTrait;
 
-class NameAuthenticator extends AbstractAuthenticator
+class NameAuthenticator extends AbstractAuthenticator implements AuthenticationEntryPointInterface
 {
     use TargetPathTrait;
 
